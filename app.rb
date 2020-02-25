9b8b5b8fd8990981d53faf04a7ff55629d3934ea
Stephen Sanchez <stephensanchez05@gmail.com>
2020-02-25 02:36:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,11 +7,11 @@ class App < Sinatra::Base
   end
   
   get '/hometown' do
-    "My hometown is"
+    "My hometown is #{params['hometown']}"
   end
   
   get '/favorite-song' do
-    "My favorite song is"
+    "My favorite song is #{params['favorite-song']}"
   end 
 
   
